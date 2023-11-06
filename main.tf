9bb9e4ce9f619132cb5bd3fe610d42f5da08fffd
sustaitangel <sustaita_7_7@hotmail.com>
2023-11-06 19:48:40 +0100

    Second Commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "aws_s3_bucket" "proveedores" {
+  count  = 3
+  bucket = "proveedores-${random_string.sufijo[count.index].id}"
+  tags = {
+    Owner       = "Sustaita"
+    Environment = "Dev"
+    Office      = "proveedores"
+  }
+}
+
+resource "random_string" "sufijo" {
+  count   = 3
+  length  = 8
+  special = false
+  upper   = false
+  numeric = false
+}
